bbef8621de4661ee09ccf83ef544b627f732e39c
Monirul <engrmonirulislam512@gmail.com>
2023-12-20 22:21:37 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/interviewtask1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.interviewtask1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/control/utils/theme_data.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData themeData() {
+  return ThemeData(
+    useMaterial3: true,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    scaffoldBackgroundColor: Colors.white,
+  );
+}
